e0565667aee3a4a718435410055e78781f86f44c
badjou2002 <bil602u@gmail.com>
2024-04-23 17:59:02 +0100

    first commit

--- app.js
+++ app.js
@@ -14,35 +14,35 @@ app.get("/", (req, res) => {
     res.status(200).send("bonjour");
 })
 
-const userRouter = require("../routes/user.route")
+const userRouter = require("./routes/user.route")
 app.use('/api/user', userRouter);
-const specialiteRouter = require("../routes/specialite.route")
+const specialiteRouter = require("./routes/specialite.route")
 app.use('/api/specialite', specialiteRouter);
-const niveauRouter = require("../routes/niveau.route")
+const niveauRouter = require("./routes/niveau.route")
 app.use('/api/niveau', niveauRouter);
-const espaceCoursRouter = require("../routes/espaceCours.route")
+const espaceCoursRouter = require("./routes/espaceCours.route")
 app.use('/api/espaceCours', espaceCoursRouter);
-const espaceDepotRouter = require("../routes/espaceDepot.route")
+const espaceDepotRouter = require("./routes/espaceDepot.route")
 app.use('/api/espaceDepot', espaceDepotRouter);
-const depotRouter = require("../routes/depot.route")
+const depotRouter = require("./routes/depot.route")
 app.use('/api/depot', depotRouter);
-const banqueQuestionRouter = require("../routes/banqueQuestion.route")
+const banqueQuestionRouter = require("./routes/banqueQuestion.route")
 app.use('/api/banqueQuestion', banqueQuestionRouter);
-const commentaireRouter = require("../routes/commentaire.route")
+const commentaireRouter = require("./routes/commentaire.route")
 app.use('/api/commentaire', commentaireRouter);
-const gamificationRouter = require("../routes/gamification.route")
+const gamificationRouter = require("./routes/gamification.route")
 app.use('/api/gamification', gamificationRouter);
-const reponseRouter = require("../routes/reponse.route")
+const reponseRouter = require("./routes/reponse.route")
 app.use('/api/reponse', reponseRouter);
-const ressourceRouter = require("../routes/ressource.route")
+const ressourceRouter = require("./routes/ressource.route")
 app.use('/api/ressource', ressourceRouter);
-const resultatRouter = require("../routes/resultat.route")
+const resultatRouter = require("./routes/resultat.route")
 app.use('/api/resultat', resultatRouter);
-const testRouter = require("../routes/test.route")
+const testRouter = require("./routes/test.route")
 app.use('/api/test', testRouter);
-const testQuestionRouter = require("../routes/testQuestion.route")
+const testQuestionRouter = require("./routes/testQuestion.route")
 app.use('/api/testQuestion', testQuestionRouter);
-const themeRouter = require("../routes/theme.route")
+const themeRouter = require("./routes/theme.route")
 app.use('/api/theme', themeRouter);
 
 const PORT = process.env.PORT || 3005
